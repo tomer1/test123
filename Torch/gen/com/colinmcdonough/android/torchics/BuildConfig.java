584dc7d4244d51160b58d200f996afc9323e17b8
tmorisi <tmorisi@ISR-TMORISI.interwise.com>
2012-12-31 16:08:37 +0200

    init commit clean and fix some dumps errors

---
+++ Torch/gen/com/colinmcdonough/android/torchics/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.colinmcdonough.android.torchics;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
